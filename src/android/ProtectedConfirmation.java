8060537d677e9b793d45db5b017f00c8fa511611
Davide Bove <6752553+Dbof@users.noreply.github.com>
2019-12-04 18:13:16 +0100

    Add implementation to methods

--- src/android/ProtectedConfirmation.java
+++ src/android/ProtectedConfirmation.java
@@ -5,16 +5,28 @@ import android.security.ConfirmationAlreadyPresentingException;
 import android.security.ConfirmationCallback;
 import android.security.ConfirmationNotAvailableException;
 import android.security.ConfirmationPrompt;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.util.Base64;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 
 public class ProtectedConfirmation extends CordovaPlugin {
 
@@ -23,6 +35,9 @@ public class ProtectedConfirmation extends CordovaPlugin {
     private static final String GETCERTIFICATECHAIN = "getCertificateChain";
     private static final String PRESENTPROMPT = "presentPrompt";
     private static final String PROTECTED_CONFIRMATION_KEYALIAS = ProtectedConfirmation.class.getCanonicalName();
+
+    final String BEGIN = "-----BEGIN CERTIFICATE-----\n";
+    final String END = "\n-----END CERTIFICATE-----|";
 
 
     @Override
@@ -35,35 +50,42 @@ public class ProtectedConfirmation extends CordovaPlugin {
                     try {
                         switch(action) {
                             case ISSUPPORTED:
-                                callbackContext.success(isSupported());
+                                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isSupported()));
                                 break;
                             case INITKEY:
                                 String challenge64 = args.getString(0);
-                                callbackContext.success("");
+                                initKey(challenge64);
+                                callbackContext.success();
                                 break;
-                            case GETCERTIFICATECHAIN: break;
-                                Certificate[] chain = getCertificateChain();
-                                callbackContext.success("CHAIN TODO");
+                            case GETCERTIFICATECHAIN:
+                                byte[] chain = getCertificateChain();
+                                if (chain == null)
+                                    callbackContext.error("Could not retrieve chain. Did you call " + INITKEY + " first?");
+                                else
+                                    callbackContext.success(chain);
                                 break;
                             case PRESENTPROMPT:
-                                String promptText = args.getString(0);
-                                byte[] extraData = Base64.decode(args.getString(1), Base64.DEFAULT);
-                                presentPrompt(promptText, extraData);
-                                callbackContext.success("");
+                                // first check if signing key exists
+                                if (!keyExists()) {
+                                    callbackContext.error("Protected confirmation not yet available. Did you call " + INITKEY + " first?");
+                                } else {
+                                    String promptText = args.getString(0);
+                                    byte[] extraData = Base64.decode(args.getString(1), Base64.DEFAULT);
+                                    presentPrompt(promptText, extraData, callbackContext);
+                                }
                                 break;
                             default:
                                 callbackContext.error("Invalid method call");
-                                break;
                         }
                     } catch (Exception e) {
-                        callbackContext.error("Error occurred while performing " + action);
+                        System.err.println("Error while performing " + action);
+                        callbackContext.error("Error occurred while performing " + action + ": " + e.getMessage());
                     }
                 }
             });
         } catch (Exception e) {
             System.out.println("Error occurred while performing " + action + " : " + e.getMessage());
             callbackContext.error("Error occurred while performing " + action);
-            return false;
         }
         return true;
     }
@@ -72,26 +94,55 @@ public class ProtectedConfirmation extends CordovaPlugin {
         return ConfirmationPrompt.isSupported(cordova.getActivity().getApplicationContext());
     }
 
-    private Certificate[] getCertificateChain() throws KeyStoreException {
-        KeyStore keyStore = null;
-        keyStore = KeyStore.getInstance("AndroidKeyStore");
+    private byte[] getCertificateChain() throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException {
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+
+        Certificate[] chain;
         if (keyStore != null) {
             keyStore.load(null);
-            return keyStore.getCertificateChain(PROTECTED_CONFIRMATION_KEYALIAS);
+            chain = keyStore.getCertificateChain(PROTECTED_CONFIRMATION_KEYALIAS);
         } else {
             return null;
         }
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        for (Certificate c : chain) {
+            byte[] cert64 = Base64.encode(c.getEncoded(), Base64.NO_WRAP);
+            os.write(BEGIN.getBytes());
+            os.write(cert64);
+            os.write(END.getBytes());
+        }
+        return os.toByteArray();
     }
 
-    private void initKey(byte[] challenge) {
-        // TODO
-    }
-
-    private void initKey_b64(String challenge64) {
+    private void initKey(String challenge64) throws InvalidAlgorithmParameterException, NoSuchProviderException, NoSuchAlgorithmException {
         byte[] challenge = Base64.decode(challenge64, Base64.DEFAULT);
+        initKey(challenge);
     }
 
-    private void presentPrompt(String promptText, byte[] extraData) throws ConfirmationNotAvailableException, ConfirmationAlreadyPresentingException {
+    private void initKey(byte[] challenge) throws InvalidAlgorithmParameterException, NoSuchProviderException, NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore");
+
+        KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(PROTECTED_CONFIRMATION_KEYALIAS, KeyProperties.PURPOSE_SIGN)
+                .setUserConfirmationRequired(true)
+                .setIsStrongBoxBacked(true)
+                .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+                .setAttestationChallenge(challenge);
+        kpg.initialize(builder.build());
+        kpg.generateKeyPair();
+    }
+
+    private boolean keyExists() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+        if (keyStore != null) {
+            keyStore.load(null);
+            return keyStore.containsAlias(PROTECTED_CONFIRMATION_KEYALIAS);
+        }
+        return false;
+    }
+
+    private void presentPrompt(String promptText, byte[] extraData, final CallbackContext callbackContext) throws ConfirmationNotAvailableException, ConfirmationAlreadyPresentingException {
         Activity activity = cordova.getActivity();
 
         ConfirmationPrompt prompt = new ConfirmationPrompt.Builder(activity.getApplicationContext())
@@ -99,29 +150,33 @@ public class ProtectedConfirmation extends CordovaPlugin {
                 .setPromptText(promptText)
                 .build();
 
-        prompt.presentPrompt(activity.getMainExecutor(), createConfirmationCallback());
+        prompt.presentPrompt(activity.getMainExecutor(), createConfirmationCallback(callbackContext));
     }
 
-    private ConfirmationCallback createConfirmationCallback() {
+    private ConfirmationCallback createConfirmationCallback(final CallbackContext callbackContext) {
         return new ConfirmationCallback() {
             @Override
             public void onConfirmed(byte[] dataThatWasConfirmed) {
                 super.onConfirmed(dataThatWasConfirmed);
+                callbackContext.success(dataThatWasConfirmed);
             }
 
             @Override
             public void onDismissed() {
                 super.onDismissed();
+                callbackContext.error("Confirmation Prompt was dismissed!");
             }
 
             @Override
             public void onCanceled() {
                 super.onCanceled();
+                callbackContext.error("Confirmation Prompt was cancelled by the application!");
             }
 
             @Override
             public void onError(Throwable e) {
                 super.onError(e);
+                callbackContext.error("Error while showing Confirmation Prompt: " + e.getMessage());
             }
         };
     }
